ea6341b761489eeb89895470ccd8e4220e458b12
greazely <muhammedko420@gmail.com>
2024-08-16 19:56:00 +0100

    update

---
+++ .history/components/Footer_20240814134615.tsx
@@ -0,0 +1,28 @@
+import { AiFillLinkedin, AiFillGithub, AiFillTwitterSquare, AiFillX } from 'react-icons/ai';
+
+const Footer = () => {
+    return ( 
+        <div className="bg-gptGray px-4 py-4 md:pt-4 md:px-4 lg:px-12">
+            <div className='bg-gptGray px-4 py-4 md:pt-8 md:px-12 text-sm'>
+                <p>
+                <span className='text-2xl'>&#128187;</span> by <span className='bg-gradient-to-r from-blue-600 via-green-500 to-indigo-400 inline-block text-transparent bg-clip-text'>hammedctgr</span> for <a href="https://6ixville.github.io/" target="_blank" rel="noopener noreferrer" className='underline'>6ixville Inc.</a>
+                </p>
+                <div className="">
+                    <div className="flex justify-center space-x-4 mt-4">
+                        <a href="#" className="bg-blue-600 p-2 rounded-md shadow-deep-all">
+                            <AiFillLinkedin size={20} />
+                        </a>
+                        <a href="#" className="bg-githubGray p-2 rounded-md shadow-deep-all">
+                            <AiFillTwitterSquare size={20} />
+                        </a>
+                        <a href="#" className="bg-githubGray p-2 rounded-md shadow-deep-all">
+                            <AiFillGithub size={20} />
+                        </a>
+                    </div>
+                </div>
+            </div>
+       </div>
+     );
+}
+ 
+export default Footer;
---
+++ .history/components/ProjectsSection_20240815195654.tsx
@@ -0,0 +1,95 @@
+import { FC } from 'react';
+
+interface Project {
+  title: string;
+  description: string;
+  technologies: string[];
+  date: string;
+  status: string;
+  images: string[];
+  link: string;
+}
+
+const projects: Project[] = [
+  {
+    title: "Atease",
+    description: "Atease is a major oil and gas company that deals in all kinds crude products.",
+    technologies: ["HTML", "CSS", "Bootstrap", "Node.js", "Javascript"],
+    date: "2023",
+    status: "Completed",
+    images: ["pic1.jpg", "pic1.jpg", "pic1.jpg"],
+    link: "https://ateasegroup.com",
+  },
+  {
+    title: "Trumart",
+    description: "Trumart is a web2 marketplace that processes payment in web3, allowing minting of products as NFTs and more. Designed and developed by me.",
+    technologies: ["React.js", "Express.js", "Node.js", "MySQL"],
+    date: "2023",
+    status: "Completed",
+    images: ["pic1.jpg", "pic1.jpg", "pic1.jpg"],
+    link: "https://example.com",
+  },
+  {
+    title: "Trumart",
+    description: "Trumart is a web2 marketplace that processes payment in web3, allowing minting of products as NFTs and more. Designed and developed by me.",
+    technologies: ["React.js", "Express.js", "Node.js", "MySQL"],
+    date: "2023",
+    status: "Completed",
+    images: ["pic1.jpg", "pic1.jpg",],
+    link: "https://example.com",
+  },
+  {
+    title: "Trumart",
+    description: "Trumart is a web2 marketplace that processes payment in web3, allowing minting of products as NFTs and more. Designed and developed by me.",
+    technologies: ["React.js", "Express.js", "Node.js", "MySQL"],
+    date: "2023",
+    status: "Completed",
+    images: ["pic1.jpg", "pic1.jpg",],
+    link: "https://example.com",
+  },
+  {
+    title: "Trumart",
+    description: "Trumart is a web2 marketplace that processes payment in web3, allowing minting of products as NFTs and more. Designed and developed by me.",
+    technologies: ["React.js", "Express.js", "Node.js", "MySQL"],
+    date: "2023",
+    status: "Completed",
+    images: ["pic1.jpg", "pic1.jpg",],
+    link: "https://example.com",
+  },
+  // Add more projects as needed...
+];
+
+const ProjectsSection: FC = () => {
+  return (
+    <section className="bg-gptGray md:px-8 px-4 py-4 lg:px-12">
+      <div className="container bg-githubGray rounded-md mx-auto md:px-8">
+        <h2 className="text-2xl font-bold text-white p-4">Projects and Experience</h2>
+        <div className="grid grid-cols-1 sm:grid-cols-3 lg:grid-cols-4 gap-8 p-4">
+          {projects.map((project, index) => (
+            <div key={index} className="bg-gptGray rounded-lg overflow-hidden shadow-deep-all transform transition-transform duration-300 hover:scale-110">
+              <div className="relative w-full h-48 overflow-hidden">
+                <img src={project.image} alt={project.title} className="w-full h-full object-cover shadow-deep-all rounded-md" />
+                <span className="shadow-deep-all absolute top-2 right-2 bg-green-500 text-xs text-white px-2 py-1 rounded-md">{project.status}</span>
+              </div>
+              <div className="p-4">
+                <h3 className="text-xl font-semibold text-white mb-2">{project.title}</h3>
+                <p className="text-gray-400 text-xs mb-4">{project.description}</p>
+                <div className="flex flex-wrap gap-2 mb-4">
+                  {project.technologies.map((tech, i) => (
+                    <span key={i} className="bg-githubGray shadow-deep-all text-green-300 text-xs px-2 py-1 rounded-md">{tech}</span>
+                  ))}
+                </div>
+                <div className="flex justify-between items-center">
+                  <span className="text-gray-500 text-sm">{project.date}</span>
+                  <a href={project.link} target="_blank" rel="noopener noreferrer" className="text-blue-400 hover:underline text-sm">website link</a>
+                </div>
+              </div>
+            </div>
+          ))}
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default ProjectsSection;
