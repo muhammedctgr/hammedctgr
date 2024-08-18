128d92c6d6ed8e1f5b4686a23c440938f2895371
greazely <muhammedko420@gmail.com>
2024-08-18 12:59:32 +0100

    update

--- .history/components/Footer_20240818125722.tsx
+++ .history/components/Footer_20240818125722.tsx
@@ -5,7 +5,7 @@ const Footer = () => {
         <div className="bg-gptGray px-4 py-4 md:pt-4 md:px-4 lg:px-12">
             <div className='bg-gptGray px-4 py-4 md:pt-8 md:px-12 text-sm'>
                 <p>
-                <span className='text-2xl'>&#128187;</span><span className='text-xs'> by</span> <span className='bg-gradient-to-r from-blue-600 via-green-500 to-indigo-400 inline-block text-transparent bg-clip-text'>hammedctgr</span><span className='text-xs'> under the license of </span><a href="https://6ixville.github.io/" target="_blank" rel="noopener noreferrer" className='underline'>6ixville Inc.</a>
+                <span className='text-2xl'>&#128187;</span><span className='text-xs'> by</span> <span className='bg-gradient-to-r from-blue-600 via-green-500 to-indigo-400 inline-block text-transparent bg-clip-text'>hammedctgr</span><span></span> under the license of <a href="https://6ixville.github.io/" target="_blank" rel="noopener noreferrer" className='underline'>6ixville Inc.</a>
                 </p>
                 <div className="">
                     <div className="flex justify-center space-x-4 mt-4">
